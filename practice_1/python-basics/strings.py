52ef7f6a6bb42698838bb11184223de4821b2054
olzhasoff <o_myrzakhan@kbtu.kz>
2026-01-24 14:14:13 +0500

    update

--- practice_1/python-basics/strings.py
+++ practice_1/python-basics/strings.py
@@ -1,11 +1,14 @@
 x = 'Mike'
-print(x[3]) # M{0} i{1} k{2} e{3} 
+print(x[3]) # e, M(0) i(1) k(2) e(3)
 
 x = 'Hello, Mike'
 print(len(x)) # 11
 
 x = "Mike"
 print(x[0]) # M
+
+x = "Mike"
+print(x[-1]) # e
 
 x = "Mike"
 print(x[::-1]) # ekiM
