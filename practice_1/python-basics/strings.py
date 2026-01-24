a2670ee1937c0945d95afad7af18c704e9a763f0
olzhasoff <o_myrzakhan@kbtu.kz>
2026-01-24 14:10:59 +0500

    update

--- practice_1/python-basics/strings.py
+++ practice_1/python-basics/strings.py
@@ -1,1 +1,17 @@
+x = 'Mike'
+print(x[3]) # M{0} i{1} k{2} e{3} 
 
+x = 'Hello, Mike'
+print(len(x)) # 11
+
+x = "Mike"
+print(x[0]) # M
+
+x = "Mike"
+print(x[::-1]) # ekiM
+
+x = 'Hello, Mike'
+print("Mike" in x) # True
+
+x = 'Hello, Mike'
+print("World" not in x) # True
