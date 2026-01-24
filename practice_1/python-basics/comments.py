25905b6309bcee6512c0c677d5e305e7016c43ad
olzhasoff <o_myrzakhan@kbtu.kz>
2026-01-24 14:00:45 +0500

    update

--- practice_1/python-basics/comments.py
+++ practice_1/python-basics/comments.py
@@ -1,24 +1,5 @@
 # This is a comment
 # Comments in Python start with #
 
-# ex 1: what do we use to implement a comment? - '#' sign
-
-# ex 2: the visual code of a comment:
-
-# This is a comment, that will be ignored
-
-# ex 3, multiline comments:
-
-a = '''
-This is the multiple line comment, and this is actually a string
-
-So it can be declared and even printed
-
-''' 
-print(a)
-
-# Try it yourself
-# this is a comment
-print("Altynbek")
-
-print("Altynbek") # < -- comment can be writed only after the code, otherwise the whole line will be a comment
+a = ''' This is the multiple line comment ''' 
+print(a) 
