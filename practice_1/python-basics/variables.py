0bc8a6dedb6a8adcde538513b40221a40b6c38be
altynbex <altynbex11@gmail.com>
2026-01-25 19:49:01 +0500

    update

--- practice_1/python-basics/variables.py
+++ practice_1/python-basics/variables.py
@@ -1,4 +1,26 @@
-x = 3
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
 
 a = b = c = 4
 
